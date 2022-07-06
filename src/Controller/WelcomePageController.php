feb0ced540ee53945fdd71351ee5f457ab2a17f0
Fabio Dalmasso <fabio.dalmasso@gmail.com>
2022-07-06 16:32:45 +0200

    Update with welcome page

---
+++ src/Controller/WelcomePageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WelcomePageController extends AbstractController
+{
+    #[Route('/welcome/page', name: 'welcome_page')]
+    public function index(): Response
+    {
+        return $this->render('welcome_page/index.html.twig', [
+            'controller_name' => 'WelcomePageController',
+        ]);
+    }
+}
